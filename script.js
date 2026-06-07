8d2b72788503ba48298387429665a6557836bca5
frenz-veind <khenzievivo@gmail.com>
2026-06-07 17:13:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const lyrics = [
   { time: 0, text: "Did you think that I wouldn't see you out at the movies?" },
-  { time: 6, text: "What you doin' to me? You're taking him where we used to go" },
-  { time: 12, text: "Now if you're trying to break my heart" },
-  { time: 17, text: "It's workin','cause you know that" },
-  { time: 22, text: "That should be me, holdin' your hand" },
-  { time: 27, text: "That should be me, makin' you laugh" },
-  { time: 32, text: "That should be me, this is so sad" },
-  { time: 37, text: "That should be me, that should be me" },
-  { time: 42, text: "That should be me, feelin' your kiss" },
-  { time: 47, text: "That should be me, buyin' you gifts" },
-  { time: 52, text: "This is so wrong, I can't go on" },
-  { time: 57, text: "'Til you believe that that should be me" }
+  { time: 4, text: "What you doin' to me? You're taking him where we used to go" },
+  { time: 8, text: "Now if you're trying to break my heart" },
+  { time: 10, text: "It's workin','cause you know that" },
+  { time: 13, text: "That should be me, holdin' your hand" },
+  { time: 17, text: "That should be me, makin' you laugh" },
+  { time: 21, text: "That should be me, this is so sad" },
+  { time: 25, text: "That should be me, that should be me" },
+  { time: 28, text: "That should be me, feelin' your kiss" },
+  { time: 30, text: "That should be me, buyin' you gifts" },
+  { time: 34, text: "This is so wrong, I can't go on" },
+  { time: 38, text: "'Til you believe that that should be me" }
 ];
 
 const song = document.getElementById("song");
